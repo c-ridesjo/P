eca6feb29819b87c5a51303377d88b3e5bd9f5e2
Camilla Ridesjö <camilla.ridesjo@gmail.com>
2023-02-13 17:52:15 +0100

    Add html and css

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import './style.css'
+/*import './style.css'
 import javascriptLogo from './javascript.svg'
 import { setupCounter } from './counter.js'
 
@@ -20,4 +20,4 @@ document.querySelector('#app').innerHTML = `
   </div>
 `
 
-setupCounter(document.querySelector('#counter'))
+setupCounter(document.querySelector('#counter'))*/
